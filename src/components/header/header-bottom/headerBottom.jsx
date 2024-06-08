253f5717f6026d49afec96277b73e2487addf4b1
eldano6189 <eldano6189@gmail.com>
2024-06-09 00:38:17 +0300

    Added onclick event to bottom header to change colour when selected

--- src/components/header/header-bottom/headerBottom.jsx
+++ src/components/header/header-bottom/headerBottom.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import { bottomNav } from "../../../data/bottomNav";
 import styles from "./headerBottom.module.css";
 
 const HeaderBottom = () => {
+  const [activeLink, setActiveLink] = useState(null);
+
   return (
     <div>
       <div className={styles.container}>
@@ -9,7 +12,14 @@ const HeaderBottom = () => {
           <ul>
             {bottomNav.map((item, i) => {
               return (
-                <li key={i}>
+                <li
+                  key={i}
+                  onClick={() => setActiveLink(i)}
+                  style={{
+                    background:
+                      activeLink === i ? "var(--clr-purple-dark)" : null,
+                  }}
+                >
                   {item.icon}
                   <p>{item.name}</p>
                 </li>
